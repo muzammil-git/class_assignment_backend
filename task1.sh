fca6187f0fae87afad1a7fd9b12f43d5f2186b7f
muzammil-git <syedmuzammil67@gmail.com>
2023-12-04 02:23:46 +0500

    initial commit

--- task1.sh
+++ task1.sh
@@ -8,6 +8,7 @@ repo_name="class_assignment_backend"
 
 echo "Today is $date, $github_access_token $github_username"
 
+git pull https://github.com/muzammil-git/class_assignment_backend.git
 git add .
 git commit -m 'initial commit'
 git push https://${github_access_token}@github.com/${github_username}/${repo_name}.git origin main
